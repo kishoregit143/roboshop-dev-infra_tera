a8fa8e3ec0693b5e16e0e54a6a9d8ae6fd9a8070
kishore <pagadalamokisthu@gmail.com>
2026-02-18 12:15:42 +0530

    terraform

--- 00-vpc/00-vpc/paramaterrs.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ssm_parameter" "sg_id" {
-#   count = length(var.sg_names)
-  name  = "/${var.project_name}/${var.environment}/vpc_id"
-  type  = "String"
-  value = module.vpc.vpc_id
-}
