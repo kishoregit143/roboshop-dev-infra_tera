0ef704918b0426b17d929e837798dd7403209c2e
kishore <pagadalamokisthu@gmail.com>
2026-02-19 17:04:15 +0530

    terraform

---
+++ 30-sgrules/main.tf
@@ -0,0 +1,9 @@
+##### Backend ALB SG Rules #####
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
