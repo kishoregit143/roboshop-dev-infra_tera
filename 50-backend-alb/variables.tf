7376a10877a66caac4f2ae8e0342a3eb0d4021f4
kishore <pagadalamokisthu@gmail.com>
2026-02-19 17:04:26 +0530

    terraform

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+# variable "zone_id" {
+#     default = "Z07848402FDVN47BEURKI"
+# }
+
+# variable "domain_name" {
+#     default = "devopslab.fun"
+# }
