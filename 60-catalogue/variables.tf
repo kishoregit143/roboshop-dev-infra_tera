83bea3fd56313d05cdf249ae9d8bee14676bbe1b
kishore <pagadalamokisthu@gmail.com>
2026-03-02 12:29:31 +0530

    Initial commit - fixed repo structure

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "devopslab.fun"
+}
+variable "instance_type" {
+    default = "t3.micro"
+}
