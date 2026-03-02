6e471f00d394baaded9f491a6c8facb921f9f2ac
kishore <pagadalamokisthu@gmail.com>
2026-03-02 15:09:47 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "devopslab.fun"
+}
+variable "instance_type" {
+    default = "t3.micro"
+}
+variable "zone_id" {
+    default = "Z07848402FDVN47BEURKI"
+}
