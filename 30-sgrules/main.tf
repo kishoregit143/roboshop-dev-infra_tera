89b3d5606061a2de5db949f349d8ab2f36262227
kishore <pagadalamokisthu@gmail.com>
2026-03-03 14:14:30 +0530

    terraform

--- 30-sgrules/main.tf
+++ 30-sgrules/main.tf
@@ -280,3 +280,12 @@ resource "aws_security_group_rule" "frontend_alb_public" {
   protocol          = "tcp"
   to_port           = 443
 }
+##### Bastion SG Rules #####
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  security_group_id = local.bastion_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
