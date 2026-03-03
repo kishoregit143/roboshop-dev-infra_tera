74fcd8603a32d988eaf8ca7eb9e5a566007a7c59
kishore <pagadalamokisthu@gmail.com>
2026-03-03 12:58:40 +0530

    terraform

--- 90-componets/main.tf
+++ 90-componets/main.tf
@@ -1,12 +1,12 @@
-    module "components" {
-    source = "git::https://github.com/kishoregit143/terraform-roboshop-component.git?ref=main"
-    component = var.component
-    rule_priority = var.rule_priority
-}
- 
-# module "components" {
-#     for_each = var.components
-#     source = "git::https://github.com/daws-86s/terraform-roboshop-component.git?ref=main"
-#     component = each.key
-#     rule_priority = each.value.rule_priority
+#     module "components" {
+#     source = "git::https://github.com/kishoregit143/terraform-roboshop-component.git?ref=main"
+#     component = var.component
+#     rule_priority = var.rule_priority
 # }
+ 
+module "components" {
+    for_each = var.components
+    source = "git::https://github.com/kishoregit143/terraform-roboshop-component.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
+}
