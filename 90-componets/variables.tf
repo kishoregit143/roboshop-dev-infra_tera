52a74ed44c46bf5969c0123088f60a3238536d36
kishore <pagadalamokisthu@gmail.com>
2026-03-03 12:49:34 +0530

    terraform

--- 90-componets/variables.tf
+++ 90-componets/variables.tf
@@ -6,11 +6,11 @@ variable "rule_priority" {
     default = 10
 }
 
-variable "components" {
-    default = {
-        catalogue = {
-            rule_priority = 10
-        }
+# variable "components" {
+#     default = {
+#         catalogue = {
+#             rule_priority = 10
+#         }
     #     user = {
     #         rule_priority = 20
     #     }
