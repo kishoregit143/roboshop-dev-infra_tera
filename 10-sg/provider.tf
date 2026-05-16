ba4a1a313ee73800d721a96bafbad97eaed4b82e
kishore <pagadalamokisthu@gmail.com>
2026-05-16 09:19:09 +0530

    terraform-infra-creation

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
 backend "s3" {
-    bucket = "devopslabs-remote-state"
-    key    = "ec2-vpc-roboshop-sg"
+    bucket = "86s-remote"
+    key    = "sg"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
