2215969a50c24fc2507875897b859f18dba9e85c
kishore <pagadalamokisthu@gmail.com>
2026-05-18 13:25:58 +0530

    terraform-infra-creation

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
 backend "s3" {
-    bucket = "devopslabs-remote-state"
-    key    = "ec2-catalogue-roboshop"
+    bucket = "86s-remote"
+    key    = "ec2-catalogue"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
