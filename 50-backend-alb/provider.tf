958f1071fb2995a0544c841100f79c03a668f9b7
kishore <pagadalamokisthu@gmail.com>
2026-05-18 13:25:46 +0530

    terraform-infra-creation

--- 50-backend-alb/provider.tf
+++ 50-backend-alb/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
 backend "s3" {
-    bucket = "devopslabs-remote-state"
-    key    = "ec2-backendalb-roboshop"
+    bucket = "86s-remote"
+    key    = "ec2-backendalb"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
