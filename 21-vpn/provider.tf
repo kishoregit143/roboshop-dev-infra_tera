d6ee21ce22db845522bfd9787ca01fcc9c62f0b4
kishore <pagadalamokisthu@gmail.com>
2026-05-20 13:31:41 +0530

    terraform-infra-creation

--- 21-vpn/provider.tf
+++ 21-vpn/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
 backend "s3" {
-    bucket = "devopslabs-remote-state"
-    key    = "ec2-vpn-roboshop"
+    bucket = "86s-remote"
+    key    = "ec2-vpn"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
